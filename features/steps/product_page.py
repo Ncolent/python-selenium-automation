45c1538aa8f41b1ae8377ae3646df9121b90f14b
Ncolent <joshhbizz@gmail.com>
2023-09-25 07:59:57 -0700

    working on loop test case

--- features/steps/product_page.py
+++ features/steps/product_page.py
@@ -3,6 +3,14 @@ from selenium.webdriver.common.by import By
 
 ADD_TO_CART = (By.ID, 'add-to-cart-button')
 PRODUCT_NAME = (By.ID, 'productTitle')
+COLOR_OPTIONS = (By.CSS_SELECTOR, 'li[id*=color_name]')
+CURRENT_COLOR = (By.CSS_SELECTOR, '.selection')
+
+
+
+@given('Open Amazon product {product_id} page')
+def open_amazon_product(context, product_id):
+    context.driver.get(f'https://www.amazon.com/dp/{product_id}/')
 
 @then('Add product to cart')
 def add_product_to_cart(context):
@@ -13,3 +21,13 @@ def get_product_name(context):
     context.product_name = context.driver.find_element(*PRODUCT_NAME).text
     print(f'Current product: {context.product_name}')
 
+@then('Verify user can click through colors')
+def verify_can_click_colors(context):
+    expected_colors = COLOR_OPTIONS
+
+    colors = context.driver.find_element(*COLOR_OPTIONS)
+    print(colors)
+    print(len(colors))
+
+    for color in colors:
+        color.click()
---
+++ features/tests/product_feature.feature
@@ -0,0 +1,6 @@
+# Created by jblai at 9/25/2023
+Feature: loop test
+
+  Scenario: user can select colors
+    Given Open Amazon product B09MZBN4FP page
+    Then Verify user can click through colors
